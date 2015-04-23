3418bf18e5d4b73764d31236450b904643f8958c
Max <mhmad.hamster@gmail.com>
2015-04-23 22:30:39 +0300

    ostalnoe

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
 
-    var commentForm = $('.js-comment-form');
+    function tmpl(result, author, comment) {
+        return  '<div class="media-left"><p class="text-muted">#' + result + '</p></div>' +
+                '<div class="media-body">' +
+                    '<h4 class="media-heading">' + author + '</h4>' +
+                    '<p>' + comment + '</p>' +
+                    '<div class="comment-buttons">' +
+                        '<a href="" class="js-reply">Reply <span class="glyphicon glyphicon-retweet"></span></a>' +
+                        '<a href="" class="js-remove">Remove <span class="glyphicon glyphicon-remove"></span></a>' +
+                    '</div>' +
+                '</div>';
+    }
+
+    var commentForm = $('.js-comment-form'),
+        re = /[^\w\d\s[А-Яа-я]]/;
 
     $('.media-list').on('click', '.js-reply', function(e) {
 
@@ -38,16 +51,30 @@ $(document).ready(function() {
 
         e.preventDefault();
 
-        var commentToRemove = $(this).closest('.media');
+        var commentToRemove = $(this).closest('.media'),
+            commentId = commentToRemove.attr('id');
 
         if(commentToRemove.find(commentForm).length !== 0) {
             commentForm.hide();
             $('.js-article').append(commentForm);
         };
 
-        commentToRemove.slideUp(300, function() {
-            $(this).remove();
-        });
+        $.ajax({
+            method: 'POST',
+            url: '../app/php/delete.php',
+            data: { deleteId: commentId },
+            error: function(xhr, error) {
+                console.debug(xhr);
+                console.debug(error);
+                alert('Something has gone wrong! Check console for details.')
+            },
+            success: function(result) {
+                commentToRemove.slideUp(300, function() {
+                    $(this).remove();
+                });
+                console.log(result);
+            }
+        })
 
     });
 
@@ -58,7 +85,20 @@ $(document).ready(function() {
         e.preventDefault();
 
         var author = this.commentAuthor.value,
-            comment = this.commentText.value;
+            comment = this.commentText.value,
+            _this = $(this)
+            parent;
+
+        if (re.exec(author) || re.exec(comment)) {
+            alert("Unexpected input!");
+            return false;
+        };
+
+        if ($(this).closest('.media').closest('.media').length) {
+            parent = $(this).closest('.media').closest('.media').attr('id');
+        } else {
+            parent = 0;
+        }
 
         this.commentAuthor.value = '';
         this.commentText.value = '';
@@ -67,30 +107,27 @@ $(document).ready(function() {
             author = 'Anonymous';
         };
 
-        if ($(this).closest('.media').length === 0) {
-            $(this).closest('.js-article').find('.media-list').append('<li class="media">' +
-                                                                        '<div class="media-body">' +
-                                                                            '<h4 class="media-heading">' + author + '</h4>' +
-                                                                            '<p>' + comment + '</p>' +
-                                                                            '<div class="comment-buttons">' +
-                                                                                '<a href="" class="js-reply">Reply <span class="glyphicon glyphicon-retweet" aria-hidden="true"></span></a>' +
-                                                                                '<a href="" class="js-remove">Remove<span class="glyphicon glyphicon glyphicon-remove" aria-hidden="true"></span></a>' +
-                                                                            '</div>' +
-                                                                        '</div>' +
-                                                                       '</li>');
-        } else {
-            $(this).closest('.media-body').append('<li class="media">' +
-                                                    '<div class="media-left"></div>' +
-                                                    '<div class="media-body">' +
-                                                        '<h4 class="media-heading">' + author + '</h4>' +
-                                                        '<p>' + comment + '</p>' +
-                                                        '<div class="comment-buttons">' +
-                                                            '<a href="" class="js-reply">Reply <span class="glyphicon glyphicon-retweet" aria-hidden="true"></span></a>' +
-                                                            '<a href="" class="js-remove">Remove<span class="glyphicon glyphicon glyphicon-remove" aria-hidden="true"></span></a>' +
-                                                        '</div>' +
-                                                    '</div>' +
-                                                 '</li>');
-        };
+        $.ajax({
+            method: 'POST',
+            url: '../app/php/add.php',
+            data: { newAuthor: author, newComment: comment, newParent: parent },
+            error: function(xhr, error) {
+                console.debug(xhr);
+                console.debug(error);
+                alert('Something has gone wrong! Check console for details.')
+            },
+            success: function(result) {
+
+                if (_this.closest('.media').length === 0) {
+                    _this.closest('.js-article')
+                         .find('.media-list')
+                         .append('<li class="media" id="' + result + '">' + tmpl(result, author, comment) + '</li>');
+                } else {
+                    _this.closest('.media-body')
+                         .append('<div class="media" id="' + result + '">' + tmpl(result, author, comment) + '</li>');
+                };
+        
+        }});
 
         $(this).closest('.js-comment-form').slideUp(300);
 
